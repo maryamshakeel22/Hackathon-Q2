87b0e3e0c4ece4e35268a4f8862f438acd88b91f
Maryam Shakeel <157610901+maryamshakeel22@users.noreply.github.com>
2024-12-09 23:00:20 +0500

    Update page.tsx

--- src/app/contact/page.tsx
+++ src/app/contact/page.tsx
@@ -1,47 +1,75 @@
 import React from 'react'
 import Image from 'next/image'
 import { FaGreaterThan } from 'react-icons/fa6'
+import { FaPhone } from "react-icons/fa6";
+import { IoMdPin } from "react-icons/io";
+import { GoClockFill } from "react-icons/go";
+
 const Contact = () => {
   return (
     <><div>
-          <div className='relative'>
-              <Image src={'/bgshop.png'}
-                  alt='bgshop'
-                  width={1440}
-                  height={316}
-                  className='' />
-              <div className="absolute top-1/2 left-1/2 -translate-x-1/2 -translate-y-1/2 rounded-lg text-center py-6 md:px-10 md:py-8 lg:w-1/2">
-                  <h3 className="text-xl md:text-3xl font-bold  leading-snug mt-2">
-                      Contact
-                  </h3>
-                  <h6 className="text-lg font-semibold leading-relaxed flex justify-center text-center items-center gap-2 p-3">Home <FaGreaterThan className='' /><span className='font-light text-gray-700'>Contact</span> </h6>
-              </div>
+           <section className="bg-[url('/bgshop.png')] bg-cover bg-center py-12 md:py-16">
+        <div className="container mx-auto px-4 text-center">
+          <div className="inline-block w-16 h-16 mb-4" >
+          <Image src={'/logo.png'} alt='' width={77} height={77}/> </div>
+          <h1 className="text-3xl md:text-4xl font-medium mb-4">Contact</h1>
+          <div className="flex items-center justify-center gap-2 text-sm">
+            <a href="#" className="hover:underline">
+              Home
+            </a>
+            <span>
+              <FaGreaterThan width={20} height={20}/>
+            </span>
+            <span>Contact</span>
           </div>
+        </div>
+      </section>
           <div className="sm:flex-row flex-col pt-20 mb-12">
               <h1 className='text-center justify-center font-medium text-[30px] text-black'>
                   Get In Touch With Us
               </h1>
-              <p className="text-center text-gray-500">For More Information About Our Product & Services. Please Feel Free To Drop Us An Email. Our Staff Always Be There To Help You Out. Do Not Hesitate!</p>
+              <p className="text-center text-gray-500">For More Information About Our Product & Services. Please Feel Free To Drop Us An Email.
+                <br /> Our Staff Always Be There To Help You Out. Do Not Hesitate!</p>
           </div>
       </div><section
           id="contact"
           className="grid md:grid-cols-2 my-12 md:my-12 py-24 px-8 gap-4 relative"
       >
-              <div className="z-10">
-                  <h5 className="text-xl font-bold my-2">
-                      Let&apos;s Connect
-                  </h5>
-                  <p className="text-[#ADB7BE] mb-4 max-w-md">
-                      {" "}
-                      I&apos;m currently looking for new opportunities, my inbox is always
-                      open. Whether you have a question or just want to say hi, I&apos;ll
-                      try my best to get back to you!
-                  </p><br />
-                  <div className="pb-10 text-white">
-                      {/* <span className="bold"><p><MdEmail size={26} />Email: fatimaaparveen985@gmail.com</p> <br /></span>
-    <span className="bold"><FaPhone size={26} />Phone no:</span> 0327 1733985 */}
-                  </div>
+               {/* <div className="grid grid-cols-1 md:grid-cols-3 gap-8"> */}
+            {/* Contact Info */}
+            <div className="space-y-8">
+              <div className="flex gap-4">
+                <IoMdPin className="w-6 h-6 md:w-8 md:h-8" />
+                <div>
+                  <h3 className="font-medium text-lg md:text-xl mb-2">Address</h3>
+                  <p className="text-gray-600 text-sm md:text-base">
+                    236 5th SE Avenue, New York NY10000, United States
+                  </p>
+                </div>
               </div>
+              <div className="flex gap-4">
+                <FaPhone  className="w-6 h-6 md:w-8 md:h-8" />
+                <div>
+                  <h3 className="font-medium text-lg md:text-xl mb-2">Phone</h3>
+                  <p className="text-gray-600 text-sm md:text-base">
+                    Mobile: +(84) 546-6789
+                    <br />
+                    Hotline: +(84) 456-6789
+                  </p>
+                </div>
+              </div>
+              <div className="flex gap-4">
+                <GoClockFill  className="w-6 h-6 md:w-8 md:h-8" />
+                <div>
+                  <h3 className="font-medium text-lg md:text-xl mb-2">Working Time</h3>
+                  <p className="text-gray-600 text-sm md:text-base">
+                    Monday-Friday: 9:00 - 22:00
+                    <br />
+                    Saturday-Sunday: 9:00 - 21:00
+                  </p>
+                </div>
+              </div>
+            </div>
               <div>
                   {/* <form ref={form} onSubmit={sendEmail} className="flex flex-col "> */}
                   <div className="mb-6">
@@ -56,7 +84,7 @@ const Contact = () => {
                           type="text"
                           id="example"
                           required
-                          className="bg-white border border-[#33353F] placeholder-[#9CA2A9] text-gray-100 text-sm rounded-lg block w-full p-2.5"
+                          className="bg-white border border-[#9F9F9F] placeholder-[#9CA2A9] text-gray-100 text-sm rounded-lg block w-full p-2.5"
                           placeholder="Name" />
                   </div>
                   <div className="mb-6">
@@ -71,8 +99,23 @@ const Contact = () => {
                           type="email"
                           id="exampleInput91"
                           required
-                          className="bg-white border border-[#33353F] placeholder-[#9CA2A9] text-gray-100 text-sm rounded-lg block w-full p-2.5"
+                          className="bg-white border border-[#9F9F9F] placeholder-[#9CA2A9] text-gray-100 text-sm rounded-lg block w-full p-2.5"
                           placeholder="abc@gmail.com" />
+                  </div>
+                  <div className="mb-6">
+                      <label
+                          htmlFor="email"
+                          className="text-black block text-sm mb-2 font-medium"
+                      >
+                          Subjuct
+                      </label>
+                      <input
+                          name="text"
+                          type="text"
+                          id="exampleInput91"
+                          required
+                          className="bg-white border border-[#9F9F9F] placeholder-[#9CA2A9] text-gray-100 text-sm rounded-lg block w-full p-2.5"
+                          placeholder="This is an optional" />
                   </div>
                   <div className="mb-6">
                       <label
@@ -84,12 +127,12 @@ const Contact = () => {
                       <textarea
                           name="message"
                           id="message"
-                          className="bg-white border border-[#33353F] placeholder-[#9CA2A9] text-gray-100 text-sm rounded-lg block w-full p-2.5"
+                          className="bg-white border border-[#9F9F9F] placeholder-[#9CA2A9] text-gray-100 text-sm rounded-lg block w-full p-2.5"
                           placeholder="Let's talk about..." />
                   </div>
                   <button
                       type="submit"
-                      className=" text-black font-medium py-2.5 px-5 rounded-lg w-full"
+                      className=" texr-start bg-[#B88E2F] text-white font-medium py-2.5 px-5 rounded "
                   >
                       Send Message
                   </button>
