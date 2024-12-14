92f5d802f0b25ef2c8ec51ec07f7b91dbf895eb4
Maryam Shakeel <157610901+maryamshakeel22@users.noreply.github.com>
2024-12-14 19:45:03 +0500

    Update page.tsx

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,18 +1,20 @@
-import Image from "next/image";
 import Hero from "./homepage/hero";
 import SecTwo from "./homepage/secTwo";
-import SecThree from "./homepage/secThree";
+// import SecThree from "./homepage/secThree";
 import SecFour from "./homepage/secFour";
 import SecFive from "./homepage/secFive";
+import SecThree from "./pages/page";
 
 export default function Home() {
   return (
     <div>
       <Hero />
       <SecTwo />
+      {/* <SecThree /> */}
       <SecThree />
       <SecFour />
       <SecFive />
     </div>
   );
 }
+
