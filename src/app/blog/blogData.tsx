36301765960972ba473461d547e83f8bf57fdab0
Maryam Shakeel <157610901+maryamshakeel22@users.noreply.github.com>
2024-12-14 19:34:16 +0500

    Add files via upload

---
+++ src/app/blog/blogData.tsx
@@ -0,0 +1,23 @@
+export const BlogData = [
+    {
+        src:"/blog.png",
+        date:"14 Oct 2022",
+        admin:"Admin",
+        category:"Wood",
+        h1:"Going all-in with millennial design"
+    },
+    {
+        src:"/blog2.png",
+        date:"14 Oct 2022",
+        admin:"Admin",
+        category:"Handmade",
+        h1:"Exploring new ways of decorating"
+    },
+    {
+        src:"/blog3.png",
+        date:"14 Oct 2022",
+        admin:"Admin",
+        category:"Wood",
+        h1:"Going all-in with millennial design"
+    }
+]
